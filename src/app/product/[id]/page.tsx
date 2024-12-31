49403f4d426ae607e8691b466a2f55677e477428
AbdulAhadAfridi <hafizabdulahadkhanafidi@gmail.com>
2024-12-31 20:24:03 +0500

    first commit

---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,228 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { FaStar } from "react-icons/fa";
+import { HiMinus, HiPlus } from "react-icons/hi";
+import Header2 from "../../components/header2";
+import { PiGreaterThanBold } from "react-icons/pi";
+import Image from "next/image";
+
+export default function SingleProductPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { id } = params; // Extract 'id' from params
+  const [product, setProduct] = useState<any>(null); // To store the fetched product
+  const [error, setError] = useState<string | null>(null); // To handle errors
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/api/products`);
+        const data = await response.json();
+
+        // Find the product by ID
+        const productData = data.find((prod: any) => prod.id === parseInt(id));
+        if (!productData) {
+          setError("Product not found");
+        } else {
+          setProduct(productData);
+        }
+      } catch (err) {
+        setError("Failed to fetch product");
+      }
+    };
+
+    fetchProduct();
+  }, [id]);
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (!product) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <Header2 />
+      <div className="max-w-screen-2xl mx-auto w-[1550px] h-[100] bg-white flex space-x-6">
+        {/* Breadcrumb Navigation */}
+        <nav className="text-sm text-gray-500  flex">
+          <a
+            href="#"
+            className="text-[##9F9F9F] flex items-center gap-8 ml-[85px] text-[16px] "
+          >
+            Home <PiGreaterThanBold className="text-black" />
+          </a>
+          <a
+            href="#"
+            className="text-[##9F9F9F] flex items-center gap-8 ml-[25px] text-[16px]"
+          >
+            Shop <PiGreaterThanBold className="text-black" />
+          </a>
+        </nav>
+        <div className="flex items-center gap-8">
+          <Image
+            src="/log3.png"
+            alt="Icon"
+            width={481}
+            height={391}
+            className=" w-1 h-9 text-[#9F9F9F]"
+          />
+          <span className="text-black text-[16px] ">{product.name}</span>
+        </div>
+      </div>
+      <div>
+        {/* Product Content */}
+        <div className="max-w-screen-2xl mx-auto w-full xl:w-[1540px] xl:h-[820px] bg-white flex flex-wrap lg:flex-nowrap">
+          {/* Thumbnail Images */}
+          <div className="gap-4 mt-10 sm:mt-12 lg:mt-20 ml-6 sm:ml-12 lg:ml-24 space-y-6 sm:space-y-8 lg:space-y-10 w-full lg:w-[500px] overflow-visible flex lg:block justify-center">
+            <Image
+              src="/pro1.png"
+              alt="Thumbnail 1 - Asgaard sofa"
+              width={83}
+              height={55}
+              className="w-20 h-20 mt-8 xl:mt-0 bg-[#FFF9E5] rounded-[10px]"
+            />
+            <Image
+              src="/pro2.png"
+              alt="Thumbnail 2 - Asgaard sofa"
+              width={83}
+              height={55}
+              className="w-20 h-20 bg-[#FFF9E5] rounded-[10px]"
+            />
+            <Image
+              src="/pro3.png"
+              alt="Thumbnail 3 - Asgaard sofa"
+              width={83}
+              height={55}
+              className="w-20 h-20 bg-[#FFF9E5] rounded-[10px]"
+            />
+            <Image
+              src="/pro4.png"
+              alt="Thumbnail 4 - Asgaard sofa"
+              width={83}
+              height={55}
+              className="w-20 h-20 bg-[#FFF9E5] rounded-[10px]"
+            />
+          </div>
+
+          {/* Main Product Image */}
+          <div className="w-full sm:w-[60%] md:w-[50%] lg:w-[475px] lg:h-[600px] bg-[#FFF9E5] mt-10 sm:mt-12 lg:mt-20 rounded-[10px] flex justify-center items-center">
+            <div className="w-[90%] sm:w-[70%] md:w-[600px] lg:w-[600px] h-auto mt-8 sm:mt-16 md:mt-24 lg:mt-24 ml-0 sm:ml-[-10px] md:ml-[-20px] lg:ml-[-40px]">
+              <Image
+                src={product.image}
+                alt={product.name}
+                width={540}
+                height={400}
+                className="w-[90%] md:w-[540px] h-auto bg-transparent"
+              />
+            </div>
+          </div>
+
+          {/* Product Details */}
+          <div className="w-full lg:w-auto mt-10 sm:mt-20 px-4 lg:mr-[400px] ml-16">
+            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-[40px] font-bold text-black w-[300px]">
+              {product.name}
+            </h1>
+            <p className="text-lg sm:text-xl text-[#9F9F9F] mt-6    ">
+              {product.price}
+            </p>
+
+            {/* Rating and Review */}
+            <div className="flex items-center mt-2">
+              <div className="flex text-[#FFDA5B] space-x-2">
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+              </div>
+              <p className="text-sm sm:text-base text-gray-500 ml-2 flex items-center">
+                <Image
+                  src="/log3.png"
+                  alt="Icon"
+                  width={20}
+                  height={20}
+                  className="w-0.5 h-8 text-[#9F9F9F] items-center mr-2"
+                />
+                (5 Customer Reviews)
+              </p>
+            </div>
+
+            {/* Product Description */}
+            <p className="text-black mt-4 text-sm sm:text-base md:text-lg">
+              Setting the bar as one of the loudest speakers in its class, the
+              Kilburn is a compact, stout-hearted hero with a well-balanced
+              audio which boasts a clear midrange and extended highs for a
+              sound.
+            </p>
+
+            {/* Size Options */}
+            <div className="mt-6">
+              <h3 className="text-[#9F9F9F] font-medium mb-2">Size</h3>
+              <div className="flex space-x-3 mt-4">
+                <button className="px-4 py-2 bg-[#FBEBB5] rounded-xl text-sm sm:text-md font-semibold">
+                  L
+                </button>
+                <button className="px-4 py-2 bg-[#FAF4F4] rounded-xl text-sm sm:text-md font-semibold">
+                  XL
+                </button>
+                <button className="px-4 py-2 bg-[#FAF4F4] rounded-xl text-sm sm:text-md font-semibold">
+                  XS
+                </button>
+              </div>
+            </div>
+
+            {/* Color Options */}
+            <div className="mt-8">
+              <h3 className="text-[#816DFA] font-medium mb-2">Color</h3>
+              <div className="flex space-x-3 mt-4">
+                <button
+                  className="w-8 h-8 rounded-full bg-[#816DFA]"
+                  aria-label="Select Blue Color"
+                ></button>
+                <button
+                  className="w-8 h-8 rounded-full bg-[#000000]"
+                  aria-label="Select Yellow Color"
+                ></button>
+                <button
+                  className="w-8 h-8 rounded-full bg-[#CDBA7B]"
+                  aria-label="Select Gray Color"
+                ></button>
+              </div>
+            </div>
+
+            {/* Quantity and Add to Cart */}
+            <div className="mt-10 flex items-center flex-wrap space-x-4">
+              {/* Quantity Control */}
+              <div className="flex items-center border border-gray-300">
+                <button
+                  className="flex items-center justify-center p-2 text-black"
+                  aria-label="Decrease Quantity"
+                >
+                  <HiMinus />
+                </button>
+                <span className="px-4">1</span>
+                <button
+                  className="flex items-center justify-center p-2 text-black"
+                  aria-label="Increase Quantity"
+                >
+                  <HiPlus />
+                </button>
+              </div>
+
+              {/* Add To Cart Button */}
+              <button className="px-4 py-2 bg-white hover:bg-[#000000] text-black border-2 border-black rounded-[10px]">
+                <p className="text-black hover:text-white">Add To Cart</p>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/product/page.tsx
@@ -0,0 +1,203 @@
+"use client"
+import React from "react";
+import Header2 from "../components/header2";
+import { PiGreaterThanBold } from "react-icons/pi";
+import { BsFillGridFill } from "react-icons/bs";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { Link } from "lucide-react";
+
+
+
+const ProductPage = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('http://localhost:3000/api/products');
+      const data = await response.json();
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <>
+      <Header2 />
+      <section className="relative max-w-screen-2xl mx-auto">
+        {/* Background Image */}
+        <Image
+          src="/shopimg1.png" // Replace with your image path
+          alt="Background"
+          width={1440}
+          height={316}
+          className="w-full h-[340px] object-cover"
+        />
+
+        {/* Slightly Left-Aligned Content */}
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
+          {/* Icon */}
+          <div>
+            <Image
+              src="/logo.png" // Replace with your icon image path
+              alt="Icon"
+              width={64}
+              height={64}
+              className="w-16 h-16"
+            />
+          </div>
+
+          {/* Section Title */}
+          <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-semibold text-black mb-6">
+            Product
+          </h2>
+
+          {/* Breadcrumb */}
+          <p className="text-black text-sm flex items-center gap-x-2 mb-20">
+            <span className="text-black text-lg font-semibold">Home</span>
+            <PiGreaterThanBold className="text-black font-bold text-lg" />
+            <span className="text-lg">Product</span>
+          </p>
+        </div>
+      </section>
+      {/* Product Page */}
+      <div className="flex flex-wrap justify-between items-center max-w-screen-2xl mx-auto bg-[#FAF4F4] px-4 py-4">
+        {/* Left Section */}
+        <div className="flex flex-wrap items-center gap-4 xl:ml-10">
+          <Link href="/product/${product.id}">
+          <Image
+            src="/log1.png"
+            alt="Filter Icon"
+            className="w-8 sm:w-10"
+            width={40}
+            height={40}
+          />
+          </Link>
+          <p className="text-black text-xs sm:text-sm md:text-lg lg:text-lg">
+            Filter
+          </p>
+          <BsFillGridFill className="text-gray-600 text-xl sm:text-2xl" />
+          <Image
+            src="/log2.png"
+            alt="Icon 2"
+            className="w-5 sm:w-6"
+            width={24}
+            height={24}
+          />
+          <Image
+            src="/log3.png"
+            alt="Icon 3"
+            className="w-1 sm:w-1 h-6 sm:h-8"
+            width={24}
+            height={24}
+          />
+          <p className="text-black text-xs sm:text-sm md:text-lg lg:text-lg">
+            Showing 1-16 of 32 results
+          </p>
+        </div>
+
+        {/* Right Section */}
+        <div className="flex flex-wrap items-center gap-6 mt-4 lg:mt-0 mr-4 sm:mr-10">
+          <div className="flex items-center gap-4">
+            <p className="text-black text-xs sm:text-sm md:text-lg lg:text-lg">
+              Show
+            </p>
+            <button className="px-3 sm:px-4 py-1 sm:py-2 bg-white text-[#9F9F9F] text-xs sm:text-sm md:text-lg lg:text-lg rounded-md">
+              16
+            </button>
+          </div>
+          <div className="flex items-center gap-4">
+            <p className="text-black text-xs sm:text-sm md:text-lg lg:text-lg">
+              Sort by
+            </p>
+            <button className="px-4 sm:px-6 py-1 sm:py-2 bg-white text-[#9F9F9F] text-xs sm:text-sm md:text-lg lg:text-lg rounded-md">
+              Default
+            </button>
+          </div>
+        </div>
+      </div>
+       
+
+{/* Product Data 1 */}
+<div className="grid grid-cols-1 md:grid-cols-4 gap-8 container mx-auto text-center max-w-screen-2xl bg-white p-8">
+  {products.map((product: any) => (
+    <a href={`/product/${product.id}`} key={product.id}>
+    <div
+      className="mx-auto transform hover:scale-[1.05] duration-500 hover:cursor-pointer"
+    >
+      
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={287}
+            height={287}
+            className="mx-auto"
+          />
+          <h3 className="text-lg text-black mt-4">{product.name}</h3>
+          <p className="text-black text-xl font-semibold mt-2">{product.price}</p>
+        
+      
+    </div>
+    </a>
+  ))}
+
+        
+
+        
+        {/* Pagination Section */}
+        <div className="flex justify-center md:items-center flex-wrap space-x-4 mt-20 mb-20 w-full m md:ml-80 lg:ml-[450px]  xl:ml-[600px]">
+          <button className="w-16 h-16 rounded-[10px] flex justify-center items-center bg-[#FBEBB5] text-gray-800 font-medium">
+            1
+          </button>
+          <button className="w-16 h-16 rounded-[10px] flex justify-center items-center bg-[#FFF9E5] text-gray-800 font-medium">
+            2
+          </button>
+          <button className=" md:mt-4  lg:mt-0 w-16 h-16 rounded-[10px] flex justify-center items-center bg-[#FFF9E5] text-gray-800 font-medium">
+            3
+          </button>
+          <button className=" md:mt-4 xl:mt-0 w-16 h-16 rounded-[10px] flex justify-center items-center bg-[#FFF9E5] text-gray-800 font-medium">
+            Next
+          </button>
+        </div>
+      </div>
+
+      {/* Features Section */}
+      <div className="bg-[#FAF4F4] py-10  mx-auto max-w-screen-2xl w-[1550] h-[500] lg:h-[400] xl:h-[300] ">
+        <div className="max-w-7xl mx-auto grid grid-cols-1  lg:grid-cols-2 xl:grid-cols-3 gap-6 px-4 mt-14  ml-0 md:ml-48  lg:ml-4 xl:ml-40">
+          {/* Feature 1 */}
+          <div className="text-left  xl:ml-8">
+            <h3 className="text-yellow-500 xl:text-black text-[35px] font-semibold sm:ml-28 lg:ml-0">
+              Free Delivery
+            </h3>
+            <p className=" text-[#9F9F9F] text-sm lg:text-lg">
+              For all oders over $50, consectetur adipim scing elit.{" "}
+            </p>
+          </div>
+          {/* Feature 2 */}
+          <div className="text-left lg:ml-[-120px] xl:ml-4 ">
+            <h3 className="text-yellow-500 xl:text-black text-[35px] font-semibold sm:ml-28 lg:ml-0 ">
+              90 Days Return
+            </h3>
+            <p className=" text-[#9F9F9F] text-sm lg:text-lg ">
+              If goods have problems, consectetur adipiscing elit.
+            </p>
+          </div>
+          {/* Feature 3 */}
+          <div className="text-left  lg:mt-4 xl:mt-0 ">
+            <h3 className="text-yellow-500 xl:text-black text-[35px] font-semibold  sm:ml-28 lg:ml-0">
+              Secure Payment
+            </h3>
+
+            <p className=" text-[#9F9F9F] text-sm lg:text-lg flex xl:flex-col ">
+              100% secure payment, consectetur
+              <p className="ml-2 xl:ml-0">adipiscing elit.</p>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductPage;
